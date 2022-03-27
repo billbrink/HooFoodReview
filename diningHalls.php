d1f41b8937e0b03972791571875ed8ff2813f655
tylerwillis20 <72057380+tylerwillis20@users.noreply.github.com>
2022-03-27 19:37:08 -0400

    latest

--- diningHalls.php
+++ diningHalls.php
@@ -10,8 +10,45 @@
     <title>The Hoo Food Review</title>
 </head> 
 
+<!-- Tab stuff taken from https://www.w3schools.com/howto/howto_js_full_page_tabs.asp?msclkid=0750ebc7ae1311ec95c4ba22f2991121 -->
+
+<button class="tablink" onClick="location.href='diningHalls.php'" type="button">Dining Halls</button>
+<button class="tablink" onclick="location.href='dishes.php'" type="button">Dishes</button>
+
+<div id="Dining Halls" class="tabcontent">
+    <h3>Dining Halls</h3>
+    <p>fill this in with dining hall tables</p>
+</div>
+
+<div id="Dishes" class="tabcontent">
+    <h3>Dishes</h3>
+    <p>fill this in with dishes tables</p>
+</div>
+
 <body>
 <h1>O-Hill</h1>
+<!-- <div class="row justify-content-center">   -->
+<table class="w3-table w3-bordered w3-card-4" style="width:90%">
+  <thead>
+  <tr style="background-color:#B0B0B0">
+    <th width="25%">Day of the Week</th>        
+    <th width="25%">Open Time</th>        
+    <th width="25%">Close Time</th> 
+  </tr>
+  </thead>
+
+  <h2>Newcomb</h2>
+<!-- <div class="row justify-content-center">   -->
+<table class="w3-table w3-bordered w3-card-4" style="width:90%">
+  <thead>
+  <tr style="background-color:#B0B0B0">
+    <th width="25%">Day of the Week</th>        
+    <th width="25%">Open Time</th>        
+    <th width="25%">Close Time</th> 
+  </tr>
+  </thead>
+
+  <h3>Runk</h3>
 <!-- <div class="row justify-content-center">   -->
 <table class="w3-table w3-bordered w3-card-4" style="width:90%">
   <thead>
